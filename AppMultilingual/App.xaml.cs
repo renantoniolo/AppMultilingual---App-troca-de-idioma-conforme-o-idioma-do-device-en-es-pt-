f51e0d32afb0eed20380320524a94156f92f6cab
Renan <renantoniolo@hotmail.com>
2018-02-25 20:27:42 -0300

    App Multilingual - Aplicativo que troca o idioma conforme o idioma selecionado no device
    App troca o idioma, conforme o idioma que esta configurado, no device,
    para este aplicativo ele funciona com o idioma Português, Inglês e
    Espanhol.

---
+++ AppMultilingual/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+using Plugin.Multilingual;
+using Xamarin.Forms;
+
+namespace AppMultilingual
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            AppResources.Culture = CrossMultilingual.Current.DeviceCultureInfo;
+
+            MainPage =  new NavigationPage(new LoginViewPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ AppMultilingual/View/LoginViewPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AppMultilingual
+{
+    public partial class LoginViewPage : ContentPage
+    {
+        public LoginViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
